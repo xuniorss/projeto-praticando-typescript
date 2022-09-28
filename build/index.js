9b96006181aad86b545bb7e32f909b03ffe7c1ae
Junior Fortunato <fortunatojunior2020@gmail.com>
2022-09-27 21:09:44 -0300

    initial commit

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const input1 = document.getElementById('num1');
+const input2 = document.getElementById('num2');
+const btnSomar = document.getElementById('btnSomar');
+const btnSubtrair = document.getElementById('btnSubtrair');
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const input1 = document.getElementById('num1') as HTMLInputElement
+const input2 = document.getElementById('num2') as HTMLInputElement
+const btnSomar = document.getElementById('btnSomar') as HTMLElement
+const btnSubtrair = document.getElementById('btnSubtrair') as HTMLElement
