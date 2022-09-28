77fcb0cfe70d4b954c6c5d4e83da9e64ee6b4211
Junior Fortunato <fortunatojunior2020@gmail.com>
2022-09-27 21:15:59 -0300

    criando operacoes

--- build/index.js
+++ build/index.js
@@ -3,3 +3,13 @@ const input1 = document.getElementById('num1');
 const input2 = document.getElementById('num2');
 const btnSomar = document.getElementById('btnSomar');
 const btnSubtrair = document.getElementById('btnSubtrair');
+const somar = (a, b) => { return a + b; };
+const subtrair = (a, b) => { return a - b; };
+btnSomar.addEventListener('click', function () {
+    const result = somar(Number(input1.value), Number(input2.value));
+    console.log(result);
+});
+btnSubtrair.addEventListener('click', function () {
+    const result = subtrair(Number(input1.value), Number(input2.value));
+    console.log(result);
+});
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,16 @@ const input1 = document.getElementById('num1') as HTMLInputElement
 const input2 = document.getElementById('num2') as HTMLInputElement
 const btnSomar = document.getElementById('btnSomar') as HTMLElement
 const btnSubtrair = document.getElementById('btnSubtrair') as HTMLElement
+
+const somar = (a: number, b: number): number => { return a + b }
+const subtrair = (a: number, b: number): number => { return a - b }
+
+btnSomar.addEventListener('click', function() {
+    const result = somar(Number(input1.value), Number(input2.value))
+    console.log(result)
+})
+
+btnSubtrair.addEventListener('click', function() {
+    const result = subtrair(Number(input1.value), Number(input2.value))
+    console.log(result)
+})
